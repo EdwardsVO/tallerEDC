3ccc35883fb7d9f83682a38d10fb72332ed894c5
Edward Vergel <edwards.svo@gmail.com>
2021-03-01 19:56:21 -0400

    firebase have been initialized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorage } from '@angular/fire/storage';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuth,
+    AngularFireStorage,
+    AngularFirestore
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyANrSwapl7CEOGxF4Rr-oWkRj3YxQdiYJ8",
+    authDomain: "talleredc-8704c.firebaseapp.com",
+    projectId: "talleredc-8704c",
+    storageBucket: "talleredc-8704c.appspot.com",
+    messagingSenderId: "829885697499",
+    appId: "1:829885697499:web:d650cc672a12dd8ec4ebfe",
+    measurementId: "G-HLZZK446TP"
+  }
 };
 
 /*
