82fe8924250a428576cda666225f3ceef841ab2b
Edward Vergel <edwards.svo@gmail.com>
2021-03-04 13:38:36 -0400

    Angular Material have been implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module'
 
 @NgModule({
   declarations: [
@@ -19,7 +21,9 @@ import { AppComponent } from './app.component';
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireAuth,
     AngularFireStorage,
-    AngularFirestore
+    AngularFirestore,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+
+import { MatButton } from '@angular/material/button'
+
+@NgModule({
+
+    imports: [ 
+        MatButton 
+    ],
+    exports: [
+        MatButton
+    ]
+
+})
+
+export class MaterialModule {}
