6904359bae1636c5c3706330a1c562358f24a2d3
anaiturbec <anaisabeliturbec@gmail.com>
2021-03-10 16:51:20 -0400

    user page for admin view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/client/login-page/login-page.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { UserPageComponent } from './pages/admin/user-page/user-page.component';
 
 const routes: Routes = [
   { path: 'home', component: LandingPageComponent },
   { path: 'login', component: LoginPageComponent },
   { path: 'register', component: RegisterPageComponent },
+  { path: 'user', component: UserPageComponent },
   { path: '**', redirectTo: '/home' },
 ];
 
