1d86d532868d580b82147805383ae964a66967aa
Sebastian Vivas <svivas@correo.unimet.edu.ve>
2021-03-11 23:40:52 -0400

    some of the crud and firebase database config

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -4,6 +4,8 @@ import firebase from 'firebase'
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-login',
@@ -14,6 +16,7 @@ export class LoginComponent implements OnInit {
   @Output() sendFormEvent = new EventEmitter;
   authForm: FormGroup 
   user: firebase.User;
+  prueba: string;
 
 
   constructor(private _fb: FormBuilder, private authService: AuthService, private _router: Router) {
@@ -49,6 +52,8 @@ export class LoginComponent implements OnInit {
       await this.authService.getCurrentUser().subscribe(
         user => {
           this.user = user;
+          this.prueba = user.uid;
+          console.log(this.prueba);
           this._router.navigate(['/user'], {queryParams: {login: 'true'}, queryParamsHandling: 'merge'
         })})
       }
@@ -56,4 +61,5 @@ export class LoginComponent implements OnInit {
       console.log(err)
     }
   }
+
 }
