3b20468818e1e909c479aaedd4f214a2e7aa7b74
Sebastian Vivas <svivas@correo.unimet.edu.ve>
2021-03-11 21:24:20 -0400

    listo

--- src/app/components/profile-vehicle/profile-vehicle.component.ts
+++ src/app/components/profile-vehicle/profile-vehicle.component.ts
@@ -15,11 +15,15 @@ export class ProfileVehicleComponent implements OnInit {
       year: "2020",
       license_plate: "RAL38K"
     },
-  
+    {
+      img: "assets/corolla.png",
+      serial: "JHLRD77875C027456",
+      model: "Corolla",
+      year: "2020",
+      license_plate: "RAL38K"
+    },
+ 
   ]
-
-  
-
   constructor() { }
 
   ngOnInit(): void {
