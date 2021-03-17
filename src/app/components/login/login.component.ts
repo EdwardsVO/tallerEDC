0517d6b0b73d35c38597ab6149d20468577d4289
Isabella Abeledo <isabella.abeledo@correo.unimet.edu.ve>
2021-03-17 16:58:31 -0400

    modal

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,17 @@
 import { Route } from '@angular/compiler/src/core';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import firebase from 'firebase'
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/models/user';
 import { collapseTextChangeRangesAcrossMultipleVersions } from 'typescript';
 import { CrudService } from 'src/app/services/crud.service';
+import { MatDialog } from '@angular/material/dialog';
+import { RegisterComponent } from '../register/register.component';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-login',
@@ -24,15 +28,20 @@ export class LoginComponent implements OnInit {
   id: string;
   phone: string = '';
   role: string = 'client';
+  closeResult = '';
 
 
 
-  constructor(private _fb: FormBuilder, private _authService: AuthService, private _router: Router, private _db: CrudService) {
+  constructor(private _fb: FormBuilder, private _router: Router,private modalService: NgbModal ,private _authService: AuthService, private _db: CrudService) {
    }
    
+   
+
   ngOnInit(): void {
   this.createAuthForm();
+  this.createRegistrationForm();
   }
+
   
   createAuthForm(): void {
     this.authForm = this._fb.group({
@@ -94,6 +103,53 @@ export class LoginComponent implements OnInit {
     } 
   }
 
-  
 
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  createRegistrationForm(): void {
+    this.authForm = this._fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      name: ['', Validators.required],
+      phone: ['', Validators.required] 
+    })
+  }
+
+  async handleRegistration(): Promise<void> {
+    try{
+      await this._authService.registerNewUser(this.authForm.get('email').value, this.authForm.get('password').value)
+      this._authService.getCurrentUser().subscribe( res => {
+        this.id = res.uid
+        this._db.newUser(
+        this.id,
+        this.authForm.get('name').value, 
+        this.authForm.get('email').value,
+        this.authForm.get('phone').value,
+        this.role,
+      )
+    })
+    }catch(err) {
+      console.log(err);
+      console.log(this.authForm.get('email'));
+    }
+  }
 }
+
+
