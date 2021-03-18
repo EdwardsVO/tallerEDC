40536d66d6060e1b22ee25933c98de282e2b75df
anaiturbec <anaisabeliturbec@gmail.com>
2021-03-18 00:27:38 -0400

    client reports design done

--- src/app/components/client-reports/client-reports.component.ts
+++ src/app/components/client-reports/client-reports.component.ts
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-reports.component.scss']
 })
 export class ClientReportsComponent implements OnInit {
-
+  clients =[
+    {
+      img: "assets/img/placeholder.jpg",
+      name: "Ana Iturbe",
+      phone:"414-101-3177"
+    },
+    {
+      img: "assets/img/placeholder.jpg",
+      name: "Ana Iturbe",
+      phone:"414-101-3177"
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
