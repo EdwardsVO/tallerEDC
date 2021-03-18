b56b8fb408f874cc375203335549e860899bad91
anaiturbec <anaisabeliturbec@gmail.com>
2021-03-18 11:32:12 -0400

    vehicle reports design done

--- src/app/components/vehicle-reports/vehicle-reports.component.ts
+++ src/app/components/vehicle-reports/vehicle-reports.component.ts
@@ -6,7 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vehicle-reports.component.scss']
 })
 export class VehicleReportsComponent implements OnInit {
-
+  cars = [
+    {
+      img: "assets/corolla.png",
+      serial: "JHLRD77875C027456",
+      model: "Corolla",
+      year: "2020",
+      license_plate: "RAL38K",
+      time: "8:15am"
+    },
+    {
+      img: "assets/yaris.png",
+      serial: "VIHRD19374C048203",
+      model: "Yaris",
+      year: "2018",
+      license_plate: "RJA27C",
+      time: "7:15am"
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
