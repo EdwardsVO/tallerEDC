11f185be22c74709ff22a5a939454f5d2da243db
Isabella Abeledo <isabella.abeledo@correo.unimet.edu.ve>
2021-03-20 10:53:56 -0400

    mechanic page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ import { OrdersPageComponent } from './pages/manager/orders-page/orders-page.com
 import { ReportsPageComponent } from './pages/manager/reports-page/reports-page.component';
 import { AdminGuard } from './guards/admin.guard';
 import { Error404Component } from './pages/error404/error404.component';
+import { MechanicPageComponent } from './pages/mechanic-page/mechanic-page.component';
 
 
 const routes: Routes = [
@@ -30,6 +31,7 @@ const routes: Routes = [
   { path: 'user/appointments', component: AppointmentsComponent, canActivate: [UserGuard] },
   { path: 'user',  component: ProfilePageComponent, canActivate: [UserGuard] },
   { path: 'forgotPassword', loadChildren: () => import('./components/forgot-password/forgot-password.module').then(m => m.ForgotPasswordModule) },
+  { path:'mechanic', component: MechanicPageComponent},
   { path: 'error404', component: Error404Component },
   { path: '**', redirectTo: '/home' },
 ];
