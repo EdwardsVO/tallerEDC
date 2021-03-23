f524c2e485a93fb84db0799a9c239ec1b33153ba
anaiturbec <anaisabeliturbec@gmail.com>
2021-03-23 08:42:30 -0400

    our team page

--- src/app/components/our-team/our-team.component.ts
+++ src/app/components/our-team/our-team.component.ts
@@ -6,8 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./our-team.component.scss']
 })
 export class OurTeamComponent implements OnInit {
+  mates = [
+    {
+      img:"assets/img/placeholder.jpg",
+      name:"Edward",
+      last: "Vergel",
+      fact: "FOUNDER",
+      click: " window.open('http://www.google.com', '_blank'); return false;"
+    },
+    {
+      img:"assets/img/placeholder.jpg",
+      name:"Sebastian",
+      last: "Vivas",
+      fact: "FOUNDER",
+    },
+    
+    {
+      img:"assets/img/placeholder.jpg",
+      name:"Ana",
+      last: "Iturbe",
+      fact: "FOUNDER",
+    },
+    {
+      img:"assets/img/placeholder.jpg",
+      name:"Francesca",
+      last: "Marinuzzi",
+      fact: "FOUNDER",
+    },
+    {
+      img:"assets/img/placeholder.jpg",
+      name:"Isabella",
+      last: "Abeledo",
+      fact: "FOUNDER",
+    },
+    
+  ]
 
   constructor() { }
+
+  openAna(){
+    window.open("https://www.instagram.com/anaisabeliturbec/", "_blank")
+  }
 
   ngOnInit(): void {
   }
