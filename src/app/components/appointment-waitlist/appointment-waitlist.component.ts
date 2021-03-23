72de372adacb07650cd0e71325ceb4fd0f6805e2
Edward Vergel <edwards.svo@gmail.com>
2021-03-23 00:22:24 -0400

    vehicle crud and manager datepicker updated

--- src/app/components/appointment-waitlist/appointment-waitlist.component.ts
+++ src/app/components/appointment-waitlist/appointment-waitlist.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { VehiclesCrudService } from 'src/app/services/vehicles-crud.service';
 
 @Component({
   selector: 'app-appointment-waitlist',
@@ -8,9 +9,11 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class AppointmentWaitlistComponent implements OnInit {
   ownerName: string;
+  appointmentDate: string;
+  appointmentHour: string;
   cars = [];
 
-  constructor(private _firestore: AngularFirestore) { }
+  constructor(private _firestore: AngularFirestore, private _crudVeh: VehiclesCrudService) { }
 
   ngOnInit(): void {
     this.getCarsToRepair();
@@ -22,20 +25,24 @@ export class AppointmentWaitlistComponent implements OnInit {
         return {
           id: e.payload.doc.id,
           serial: e.payload.doc.data().serial,
-          marca: e.payload.doc.data().marca,
-          modelo: e.payload.doc.data().modelo,
+          brand: e.payload.doc.data().marca,
+          model: e.payload.doc.data().modelo,
           year: e.payload.doc.data().year,
-          placa: e.payload.doc.data().placa,
+          plate: e.payload.doc.data().placa,
           reparation: e.payload.doc.data().needsReparation,
+          appointmentDate: e.payload.doc.data().appointmentDate,
+          appointmentHour: e.payload.doc.data().appointmentHour,
           owner: e.payload.doc.data().owner        
         }
-        
       })
     })
   }
 
-  confirmAppointment(){
-    console.log('mamawebo')
+  confirmAppointment(carId: string){
+    this._crudVeh.updateCarAppointmentDate(carId, this.appointmentDate)
+    this._crudVeh.updateCarAppointmentHour(carId, this.appointmentHour)
+    this._crudVeh.updateCarAppointmentStatus(carId, true);
+
   }
 
 
