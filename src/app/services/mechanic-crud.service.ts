dbdd2d41ad57ea92f074ea4abbf1af12d4289cb1
Sebastian Vivas <svivas@correo.unimet.edu.ve>
2021-03-24 17:03:31 -0400

    reporte del mecanico listo

--- src/app/services/mechanic-crud.service.ts
+++ src/app/services/mechanic-crud.service.ts
@@ -14,4 +14,29 @@ export class MechanicCrudService {
     })
   }
 
+  async updateAppointment(appointment) {
+    try{
+      await this._firestore.collection('appointments').doc(appointment.id).update({
+
+        carColor: appointment.carColor,
+        carKm: appointment.carKm,
+        carGas: appointment.carGas,
+
+        // CheckBox
+        extraTire: appointment.extraTire,
+        keys: appointment.keys  ,
+        tools: appointment.tools ,
+        stereo: appointment.stereo ,
+        scratches: appointment.scratches ,
+        gato: appointment.gato ,
+
+        // Diagnostico
+        diagnostic: appointment.diagnostic,
+
+      })
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
 }
